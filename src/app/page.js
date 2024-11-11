f75e04c07e0ce1007c98c564df3aadfad9df38b8
chauveau.alexis@hotmail.fr <92083099+Drakinfer@users.noreply.github.com>
2024-11-11 23:24:24 +0100

    add Footer and FontAwesome

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,14 @@
 import Image from "next/image";
+import Footer from './components/Footer'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <>
+      <main className="">
       
-    </main>
+      </main>
+      <Footer/>
+    </>
+    
   );
 }
