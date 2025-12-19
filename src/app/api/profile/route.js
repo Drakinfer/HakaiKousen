194651ef4ae25f1d24c1eed44c4a73335e7c7167
chauveau.alexis@hotmail.fr <92083099+Drakinfer@users.noreply.github.com>
2025-12-19 22:21:29 +0100

    FIX - add import prisma missing in profile api route

--- src/app/api/profile/route.js
+++ src/app/api/profile/route.js
@@ -2,6 +2,7 @@ import { NextResponse } from 'next/server';
 import { getCurrentUserProfile } from '@/lib/currentUser';
 import { getServerSession } from 'next-auth';
 import { authOptions } from '../../../../lib/auth';
+import prisma from '../../../../lib/prisma';
 
 export async function GET() {
   try {
