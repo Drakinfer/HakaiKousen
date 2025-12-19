744b167433dd8e41c793c9c788b7b4d0fb160918
chauveau.alexis@hotmail.fr <92083099+Drakinfer@users.noreply.github.com>
2025-12-19 21:47:41 +0100

    fix bcrypt library

--- src/app/api/auth/reset-password/route.js
+++ src/app/api/auth/reset-password/route.js
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 import prisma from '../../../../../lib/prisma';
 import crypto from 'crypto';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 
 export async function POST(req) {
   try {
