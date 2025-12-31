840489224cec31fd52ad248254109db54de145fe
chauveau.alexis@hotmail.fr <92083099+Drakinfer@users.noreply.github.com>
2026-01-01 00:19:16 +0100

    change library page title

--- src/app/library/page.js
+++ src/app/library/page.js
@@ -23,7 +23,7 @@ export default function LibraryPage() {
       <main className="items-center flex md:h-main-footer justify-center p-4 mb-50 md:mb-0">
         <div className="h-full flex flex-col gap-3">
           <header className="flex items-center justify-between">
-            <h1 className="text-xl sm:text-2xl font-semibold">Library</h1>
+            <h1 className="text-xl sm:text-2xl font-semibold">Bibliothèque</h1>
             <p className="text-xs sm:text-sm text-neutral-500">
               {documents.length} document{documents.length > 1 ? 's' : ''}
             </p>
