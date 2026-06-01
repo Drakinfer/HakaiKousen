d6c66e947f32ba5830c188f4bc93def8f33dabe1
chauveau.alexis@hotmail.fr <92083099+Drakinfer@users.noreply.github.com>
2026-06-01 21:15:29 +0200

    change sparticuz/chromium

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  serverExternalPackages: ['@sparticuz/chromium'],
   experimental: {
     appDir: true,
     outputFileTracingIncludes: {
